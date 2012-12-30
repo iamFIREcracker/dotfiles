3f1bf4e4cddc97df7586282252fae174ea66ad6c
Matteo Landi <matteo@matteolandi.net>
2012-12-30 17:05:35 +0100

    Add tweaks for the apple magic mouse

--- home/bin/trackpad_settings.sh
+++ home/bin/trackpad_settings.sh
@@ -1,25 +1,36 @@
-# Enable tapping
-synclient TapButton1=1 TapButton2=3 TapButton3=2
+# Apple Magic Trackpad
 
-# Increase tap-n-drag timeout time
-#synclient SingleTapTimeout=360 FastTaps=1
+if [ xinput list 'Apple Magic Trackpad' &> /dev/null ]; then
+    # Enable tapping
+    synclient TapButton1=1 TapButton2=3 TapButton3=2
 
-# Enable horizontal scrolling
-synclient HorizTwoFingerScroll=1
+    # Increase tap-n-drag timeout time
+    #synclient SingleTapTimeout=360 FastTaps=1
 
-# Speed
-synclient MinSpeed=1 MaxSpeed=2.5 AccelFactor=0.5
+    # Enable horizontal scrolling
+    synclient HorizTwoFingerScroll=1
 
-## Pressure sensitivity
-#synclient FingerHigh=50
+    # Speed
+    synclient MinSpeed=1 MaxSpeed=2.5 AccelFactor=0.5
 
-synclient ResolutionDetect=1
+    ## Pressure sensitivity
+    #synclient FingerHigh=50
 
-# Natural scrolling
-#synclient VertScrollDelta=-157
-#synclient HorizScrollDelta=-250
-id=`xinput list | grep 'Apple Wireless Trackpad' | sed 's/.*id=\([0-9]*\).*/\1/'`
-xinput set-prop $id "Synaptics Scrolling Distance" -157 -250
+    synclient ResolutionDetect=1
 
-# Make pointer go 8x for each traversed pixel
-xset m 4 1
+    # Natural scrolling
+    #synclient VertScrollDelta=-157
+    #synclient HorizScrollDelta=-250
+    id=`xinput list | grep 'Apple Wireless Trackpad' | sed 's/.*id=\([0-9]*\).*/\1/'`
+    xinput set-prop $id "Synaptics Scrolling Distance" -157 -250
+
+    xset m 4 1
+fi
+
+# Apple Magic Mouse
+if [ xinput list 'Apple Magic Mouse' &> /dev/null ]; then 
+    xinput set-prop 'Apple Magic Mouse' 'Device Accel Constant Deceleration' 1
+    xinput set-prop 'Apple Magic Mouse' 'Device Accel Adaptive Deceleration' 1
+    xinput set-prop 'Apple Magic Mouse' 'Device Accel Velocity Scaling' 1
+    xset m 1 1
+fi
