0d82724b7e1d4179ee403ad2934941967d09418e
Matteo Landi <matteo@matteolandi.net>
2020-01-22 21:24:46 +0100

    Other configs and bins

---
+++ .lisp/init.lisp
@@ -0,0 +1,14 @@
+;;; The following lines added by ql:add-to-init-file:
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+;;; General
+(setf *print-escape* t
+      *print-circle* t
+      *print-pretty* t)
+
+;;; Debugging
+; (load "~/.lisp/pmdb.lisp")
