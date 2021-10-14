8fceb8c8e6e51caa0b79af2024a148910d7f8977
Matteo Landi <matteo@matteolandi.net>
2021-10-14 09:28:15 +0200

    Few Lisp initialization script niceties
    - Add CLS to clear the scree -- this actually sends the Right escape
      sequencies to achieve that
    - Adds the sharp-semicolon-reader, so that you can add `#;` anywhere in
      a file, and the reader would stop right there, ignoring everything
      else -- quite useful to add scratch forms to a file
    - Adds the current directory to ASDF:*CENTRAL-REGISTRY* to make it
      easier to load locally defined systems

--- .lisp/init.lisp
+++ .lisp/init.lisp
@@ -10,5 +10,26 @@
       *print-circle* t
       *print-pretty* t)
 
+(defun cls ()
+  "Clears the screen, and move the cursor to the top.
+
+    Outputs:
+
+    - \033[2J: ANSI sequence to clear the screen
+    - \033[H: ANSI sequence to move the cursor to the _home_ position
+
+    It does using some FORMAT magic (e.g. `~:*` to move back in the format argument list)"
+  (format t "~A[2J~:*~A[H" #\escape))
+
+;;;; Scratch Marker
+(defun sharp-semicolon-reader (stream sub-char numarg)
+  (declare (ignore sub-char numarg))
+  (loop :while (read-line stream nil nil))
+  (values))
+(set-dispatch-macro-character #\# #\; #'sharp-semicolon-reader)
+
 ;;; Debugging
 (load "~/.lisp/pmdb.lisp")
+
+;;; Stop symlinking -- https://www.reddit.com/r/Common_Lisp/comments/lx6al4/loading_an_asdf_system_from_current_directory/gplgpww/
+(pushnew '*default-pathname-defaults* asdf:*central-registry*)
