acde639b63b3f3124a4fba742323726b2f35ee6a
Matteo Landi <matteo@matteolandi.net>
2022-06-12 10:35:06 +0200

    Review window management
    - Reduce the number of terminal related bindings: one for the main one
      (the fullscreen one); one for the smaller one (the one I usually
      _plan_ from)
    - y/h/n to move windows to the left
    - u/j/m to move windows to the right

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -12,7 +12,7 @@ end
 --- Quick open applications
 local hyper = {"ctrl", "cmd", "alt", "shift"}
 
-function open(name)
+function switchTo(name)
   return function()
     local frontmost = hs.application.frontmostApplication()
     if name == frontmost:name() then
@@ -22,28 +22,41 @@ function open(name)
       -- https://github.com/Hammerspoon/hammerspoon/issues/1984#issuecomment-481272133
       hs.eventtap.keyStroke({"cmd"}, "`")
     else
-      -- Ohterwise... lkaunch or focus
+      -- Ohterwise... launch or focus
       hs.application.launchOrFocus(name)
-      if name == 'Finder' then
-        hs.appfinder.appFromName(name):activate()
-      end
+      -- if name == 'Finder' then
+      --   hs.appfinder.appFromName(name):activate()
+      -- end
     end
   end
+end
+
+function launchNew(name)
+    return function()
+        hs.appfinder.appFromName(name):activate()
+        hs.eventtap.keyStroke({"cmd"}, "n")
+    end
 end
 
 hs.hotkey.bind(hyper, "delete", function()
   hs.reload()
 end)
-hs.hotkey.bind(hyper, ";", open("/usr/local/bin/gimp"))
-hs.hotkey.bind(hyper, "e", open("Finder"))
-hs.hotkey.bind(hyper, "o", open("Spotify"))
-hs.hotkey.bind(hyper, "k", open("Brave Browser"))
-hs.hotkey.bind(hyper, "p", open("Alacritty-main"))
-hs.hotkey.bind(hyper, "j", open("Alacritty-fullscreen"))
-hs.hotkey.bind(hyper, "h", open("Alacritty"))
+-- hs.hotkey.bind(hyper, ";", switchTo("/usr/local/bin/gimp"))
+hs.hotkey.bind(hyper, "e", switchTo("Finder"))
+hs.hotkey.bind(hyper, "o", switchTo("Spotify"))
+hs.hotkey.bind(hyper, "k", switchTo("Google Chrome"))
+-- hs.hotkey.bind(hyper, "k", switchTo("Brave Browser"))
+-- hs.hotkey.bind(hyper, "k", switchTo("Firefox"))
+-- Keep it simple for now:
+-- - one for the plan file
+-- - one for...everything else
+-- hs.hotkey.bind(hyper, "h", switchTo("Alacritty"))
+hs.hotkey.bind(hyper, "1", launchNew("Alacritty-main"))
+hs.hotkey.bind(hyper, "p", switchTo("Alacritty-main"))
+hs.hotkey.bind(hyper, "j", switchTo("Alacritty-fullscreen"))
 
-hs.hotkey.bind(hyper, "m", whenWorking(open("Microsoft Outlook")))
-hs.hotkey.bind(hyper, "i", whenWorking(open("Microsoft Teams"), open("Discord")))
+hs.hotkey.bind(hyper, "m", whenWorking(switchTo("Microsoft Outlook")))
+hs.hotkey.bind(hyper, "i", whenWorking(switchTo("Microsoft Teams"), switchTo("Discord")))
 
 --- Resize windows
 hs.window.animationDuration = 0
@@ -64,22 +77,16 @@ function resize_pct(x, y, width, height)
   end
 end
 
-hs.hotkey.bind(hyper2, "k", resize_pct(0, 0, 1, 1))
-hs.hotkey.bind(hyper2, "j", resize_pct(1/5, 1/10, 3/5, 1))
-
+-- Left: main, half, small
+hs.hotkey.bind(hyper2, "y", resize_pct(0, 0, 3/4, 1))
 hs.hotkey.bind(hyper2, "h", resize_pct(0, 0, 1/2, 1))
-hs.hotkey.bind(hyper2, "l", resize_pct(1/2, 0, 1/2, 1))
-
-hs.hotkey.bind(hyper2, "y", resize_pct(0, 0, 1/4, 1))
-hs.hotkey.bind(hyper2, "o", resize_pct(1/4, 0, 3/4, 1))
-
-hs.hotkey.bind(hyper2, "i", resize_pct(0, 0, 1, 1/2))
-hs.hotkey.bind(hyper2, "m", resize_pct(0, 1/2, 1, 1/2))
-
-hs.hotkey.bind(hyper2, "u", resize_pct(1/8, 0, 3/4, 1))
-hs.hotkey.bind(hyper2, "n", resize_pct(1/6, 0, 2/3, 1))
-hs.hotkey.bind(hyper2, "7", resize_pct(1/4, 0, 1/4, 1))
-hs.hotkey.bind(hyper2, "8", resize_pct(1/2, 0, 1/4, 1))
+hs.hotkey.bind(hyper2, "n", resize_pct(0, 0, 1/4, 1))
+-- Right: main, half, small
+hs.hotkey.bind(hyper2, "u", resize_pct(1/4, 0, 3/4, 1))
+hs.hotkey.bind(hyper2, "j", resize_pct(1/2, 0, 1/2, 1))
+hs.hotkey.bind(hyper2, "m", resize_pct(3/4, 0, 1/4, 1))
+-- Maximize
+hs.hotkey.bind(hyper2, "k", resize_pct(0, 0, 1, 1))
 
 ---
 hs.alert.show("HS init config... reloaded")
